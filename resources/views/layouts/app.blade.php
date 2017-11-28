095d16760ee1f96756a16fb4cc04a35a71d9b95e
Rddiazolivos <31427094+Rddiazolivos@users.noreply.github.com>
2017-11-28 17:41:38 -0300

    corecion navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 <body>
     <div class="visible-xs" id="xs-check"></div>
     <div id="app">
-        <nav class="navbar navbar-inverse navbar-static-top">
+        <nav class="navbar navbar-inverse navbar-static-top navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
 
